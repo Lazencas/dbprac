f5c63d11f2137409fd982be3dc5f4269cf5cb538
Guntae Kim <dkfkskgks@naver.com>
2023-09-10 17:18:29 +0900

    init commit

---
+++ src/main/java/com/example/dbprac/DbpracApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbpracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbpracApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dbprac/DbpracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbpracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
